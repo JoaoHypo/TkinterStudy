5a707284a220c8d8e1d5dfaf53403ff10ba6031a
JoaoHypo <jguibarboza@gmail.com>
2023-01-19 22:34:05 -0300

    Added Tkinter first hello world!

---
+++ FirstGUIHelloWord.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import pandas as pd
+
+root = Tk()
+
+#Criando um widget label
+myLabel = Label(root, text = "Hellow World!")
+#Jogando para o root
+myLabel.pack()
+
+root.mainloop()
+
