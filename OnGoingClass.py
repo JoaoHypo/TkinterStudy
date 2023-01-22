8acc956cbaabcdf79c93adbc9db3331a2b990027
João "Hypo" Barboza <jguibarboza@gmail.com>
2023-01-22 15:47:24 -0300

    For Sunday it's Okay <3

--- OnGoingClass.py
+++ OnGoingClass.py
@@ -1,4 +1,4 @@
-# Video at current time: 
+# Video at current time: https://youtu.be/YXPyB4XeYLA?t=3130
 
 from tkinter import *
 
@@ -33,16 +33,16 @@ butt7 = Button(root, text="7",bg="Black",fg ="Green",padx=40,pady=20, command=bu
 butt8 = Button(root, text="8",bg="Black",fg ="Green",padx=40,pady=20, command=button_click)
 butt9 = Button(root, text="9",bg="Black",fg ="Green",padx=40,pady=20, command=button_click)
 butt0 = Button(root, text="0",bg="Black",fg ="Green",padx=40,pady=20, command=button_click)
-buttAdd = Button(root, text="+",bg="Black",fg ="Green",padx=39,pady=20, command=button_add)
-buttClear = Button(root, text="Clear",bg="Black",fg ="Green",padx=79,pady=20, command=button_clear)
-buttEqual = Button(root, text="=",bg="Black",fg ="Green",padx=91,pady=20, command=button_equal)
+buttAdd = Button(root, text="+",bg="Black",fg ="Green",padx=40,pady=20, command=button_add)
+buttClear = Button(root, text="Clear",bg="Black",fg ="Green",padx=77,pady=20, command=button_clear)
+buttEqual = Button(root, text="=",bg="Black",fg ="Green",padx=86,pady=20, command=button_equal)
 
 
 #Alocando os botôes no grid
-butt0.grid(row=4, column=0)
+butt0.grid(row=4, column=0,columnspan=1)
 buttClear.grid(row=4, column=1,columnspan=2)
 buttEqual.grid(row=5, column=1,columnspan=2)
-buttAdd.grid(row=5, column=0)
+buttAdd.grid(row=5, column=0,columnspan=1)
 
 butt1.grid(row=3, column=0)
 butt2.grid(row=3, column=1)
